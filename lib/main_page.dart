add63671792fbce8cad6a324581f83cde105f7bf
nahidulislamshakin <shakinhabib2000@gmail.com>
2021-12-21 16:26:45 +0600

    icon added

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainPage extends StatelessWidget {
   @override
@@ -6,8 +7,8 @@ class MainPage extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF04040C),
-      ),
+          primaryColor: Color(0xFF04040C),
+          scaffoldBackgroundColor: Color(0xFF0F1335)),
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -19,34 +20,47 @@ class MainPage extends StatelessWidget {
               child: Row(
                 children: [
                   Expanded(
-                    child: container(),
+                    child: container(
+                      colors: Color(0xFF4C5065),
+                      iconContent: iconcontents(
+                          icon: FontAwesomeIcons.venus, label: 'Male'),
+                    ),
                   ),
-                  Expanded(child: container())
+                  Expanded(
+                      child: container(
+                    colors: Color(0xFF4C5065),
+                    iconContent: iconcontents(
+                        icon: FontAwesomeIcons.female, label: 'Female'),
+                  ))
                 ],
               ),
             ),
             SizedBox(
-              height: 8,
+              height: 6,
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: container(),
+                    child: container(
+                      colors: Color(0xFF4C5065),
+                    ),
                   ),
                 ],
               ),
             ),
             SizedBox(
-              height: 8,
+              height: 6,
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: container(),
+                    child: container(
+                      colors: Color(0xFF4C5065),
+                    ),
                   ),
-                  Expanded(child: container())
+                  Expanded(child: container(colors: Color(0xFF4C5065)))
                 ],
               ),
             ),
@@ -62,17 +76,44 @@ class MainPage extends StatelessWidget {
   }
 }
 
+class iconcontents extends StatelessWidget {
+  final IconData icon;
+  final String label;
+
+  iconcontents({required this.icon, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 70,
+        ),
+        SizedBox(height: 8,),
+        Text(
+          '$label',
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold ,color: Color(0xFFFFFFFF)),
+        )
+      ],
+    );
+  }
+}
+
 class container extends StatelessWidget {
-  const container({
-    Key? key,
-  }) : super(key: key);
+  final Color colors;
+  // final Icon iconContent;
+  final Widget? iconContent;
+  container({required this.colors, this.iconContent});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(5),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10), color: Color(0xFF6F7693)),
+      decoration:
+          BoxDecoration(borderRadius: BorderRadius.circular(10), color: colors),
+      child: iconContent,
     );
   }
 }
